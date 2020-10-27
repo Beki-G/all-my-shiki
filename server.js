74813ef13d20e5066aafba1a8b3e61b4e1cad7ba
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-10-26 21:31:23 -0700

    initial project set up with MERN stack

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+// const routes = require("./routes")
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+}
+
+// app.use(routes)
+
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/shikidata",  { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true } );
+
+app.listen(PORT, ()=>{
+    // eslint-disable-next-line no-console
+    console.log(`APP listening on PORT http://localhost:${PORT}`)
+});
