e5c3b69e09946e759f505200f4d84ba54208301e
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-11-13 23:10:24 -0800

    move delete button on dashboard card

--- client/src/components/DashboardModCharaCard/DashboardModCharaCard.jsx
+++ client/src/components/DashboardModCharaCard/DashboardModCharaCard.jsx
@@ -26,12 +26,13 @@ const DashboardModCharaCard = ({
   }, [dateCreated]);
 
   return (
-    <div className=" my-1 px-1 w-full overflow-hidden md:my-1 md:px-1 md:w-1/2 lg:w-1/3">
-      <div className=" items-center p-4 bg-white rounded-lg shadow dark:bg-gray-800 min-w-40 ">
+    <div className=" my-1 px-1 w-full overflow-hidden md:my-1 md:px-1 md:w-1/2 lg:w-1/3  ">
+      <DeleteCharacterButton characterId={characterId} characterType={"modified"}/>
+      <div className=" items-center p-4 bg-white rounded-lg shadow dark:bg-gray-800 min-w-40 mt-2 mr-2">
         {/* <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
                     Replace with Shiki Image
                   </div> */}
-        <DeleteCharacterButton characterId={characterId} characterType={"modified"}/>
+        
         <div className="text-left">
           <p className="mb-2 text-lg font-semibold text-gray-800 dark:text-gray-400">
             {name}
