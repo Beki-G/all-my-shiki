fa08b4f7b1acae4589faf0e50d27b7aae01d7ef8
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-11-18 21:37:10 -0800

    link character names on cards to character profiles, format DashboardCards

--- client/src/components/DashboardFavoritesCard/DashboardFavoritesCard.jsx
+++ client/src/components/DashboardFavoritesCard/DashboardFavoritesCard.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
-import { DeleteCharacterButton } from '../Buttons/DeleteCharacterButton/DeleteCharacterButton'
+import React from "react";
+import { Link } from "react-router-dom";
+import { DeleteCharacterButton } from "../Buttons/DeleteCharacterButton/DeleteCharacterButton";
 
-const DashboardFavoritesCard = ({ name, soulSetMain, soulSetSub, characterId }) => {
-    return (
-        <div className="inline-block mr-2 ml-1">
-          <DeleteCharacterButton characterId={characterId} characterType={'favorite'}/>
-            <div className=" items-center p-4 mt-2 mr-2 bg-white rounded-lg shadow dark:bg-gray-800 min-w-40">
-                  {/* <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
+const DashboardFavoritesCard = ({
+  name,
+  soulSetMain,
+  soulSetSub,
+  characterId,
+}) => {
+  return (
+    <div className="inline-block mr-2 ml-1">
+      <DeleteCharacterButton
+        characterId={characterId}
+        characterType={"favorite"}
+      />
+      <div className=" items-center p-4 mt-2 mr-2 bg-white rounded-lg shadow dark:bg-gray-800 min-w-40">
+        {/* <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
                     Replace with Shiki Image
                   </div> */}
-                  
-                  <div>
-                    <p className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-400">
-                      {name}
-                    </p>
-                    <p className="text-sm font-medium text-gray-600 dark:text-gray-200">
-                      Soul Set Main: {soulSetMain? {soulSetMain}: "N/A"}
-                    </p>
-                    <p className="text-sm font-medium text-gray-600 dark:text-gray-200">
-                      Soul Set Sub: {soulSetSub? {soulSetSub}: "N/A"}
-                    </p>
-                  </div>
-                  
-                </div>
-        </div>
-    )
-}
 
-export default DashboardFavoritesCard
+        <div>
+          <Link
+            className="mb-2 text-lg font-semibold text-gray-700 dark:text-gray-400"
+            to={`/shiki/${characterId}`}
+          >
+            {name}
+          </Link>
+          <p className="text-sm font-medium text-gray-600 dark:text-gray-200">
+            Soul Set Main: {soulSetMain ? { soulSetMain } : "N/A"}
+          </p>
+          <p className="text-sm font-medium text-gray-600 dark:text-gray-200">
+            Soul Set Sub: {soulSetSub ? { soulSetSub } : "N/A"}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardFavoritesCard;
--- client/src/components/DashboardModCharaCard/DashboardModCharaCard.jsx
+++ client/src/components/DashboardModCharaCard/DashboardModCharaCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { DeleteCharacterButton } from "../Buttons/DeleteCharacterButton/DeleteCharacterButton";
+import { Link } from "react-router-dom";
 
 const DashboardModCharaCard = ({
   name,
@@ -27,16 +28,19 @@ const DashboardModCharaCard = ({
 
   return (
     <div className=" my-1 px-1 w-full overflow-hidden md:my-1 md:px-1 md:w-1/2 lg:w-1/3  ">
-      <DeleteCharacterButton characterId={characterId} characterType={"modified"}/>
+      <DeleteCharacterButton
+        characterId={characterId}
+        characterType={"modified"}
+      />
       <div className=" items-center p-4 bg-white rounded-lg shadow dark:bg-gray-800 min-w-40 mt-2 mr-2">
         {/* <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
                     Replace with Shiki Image
                   </div> */}
-        
+
         <div className="text-left">
-          <p className="mb-2 text-lg font-semibold text-gray-800 dark:text-gray-400">
+          <Link className="mb-2 text-lg font-semibold text-gray-800 dark:text-gray-400" to={`/shiki/${characterId}`}>
             {name}
-          </p>
+          </Link>
           <p className="text-sm font-medium text-gray-600 dark:text-gray-200">
             Date Created: {dateCreated ? dateFormatted : "N/A"}
           </p>
