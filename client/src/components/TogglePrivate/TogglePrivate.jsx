0bc8b90825a729b378fa3d1bc80f02084e514241
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-11-19 14:26:05 -0800

    correct togglePrivate syntax

--- client/src/components/TogglePrivate/TogglePrivate.jsx
+++ client/src/components/TogglePrivate/TogglePrivate.jsx
@@ -3,18 +3,18 @@ import "./styles.css";
 
 const TogglePrivate = ({ onChange }) => {
   return (
-    <label for="toogleA" class="flex items-center cursor-pointer">
+    <label htmlFor="toogleA" className="flex items-center cursor-pointer">
       {/* <!-- toggle --> */}
-      <div class="relative">
+      <div className="relative">
         {/* <!-- input --> */}
-        <input id="toogleA" type="checkbox" class="hidden" />
+        <input id="toogleA" type="checkbox" className="hidden" />
         {/* <!-- line --> */}
-        <div class="toggle__line w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
+        <div className="toggle__line w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
         {/* <!-- dot --> */}
-        <div class="toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0"></div>
+        <div className="toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0"></div>
       </div>
       {/* <!-- label --> */}
-      <div class="ml-3 text-gray-700 font-medium">Set to private</div>
+      <div className="ml-3 text-gray-700 font-medium">Set to private</div>
     </label>
   );
 };
