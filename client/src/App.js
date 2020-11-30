37cb7a3a005191150a1a15bc61e079ca99c32da7
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-11-30 00:19:40 -0800

    deploy attempt #2

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { UserSessionProvider } from "./utils/UserContext";
