ede85296fecdadb6d9f7b07856f3f5b1b913386b
Izuku <63758052+Beki-G@users.noreply.github.com>
2020-11-29 22:04:10 -0800

    update modal close button

--- client/src/components/Modal/Modal.jsx
+++ client/src/components/Modal/Modal.jsx
@@ -5,10 +5,13 @@ const Modal = ({ open, children, onClose }) => {
   if (!open) return null;
   return ReactDom.createPortal(
     <>
-      <div className="overlay fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-75 z-100"></div>
-      <div className="Modal_styles fixed top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white p-20 z-100 flex flex-col">
+      <div className="overlay fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-75 z-100 rounded-md"></div>
+      <div className="Modal_styles fixed top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 bg-white p-20 z-100 flex flex-col rounded-md">
+        <div className="mb-6">
         {children}
-        <button onClick={onClose} className="block uppercase mx-auto shadow bg-indigo-800 hover:bg-indigo-700 focus:ring focus:outline-none text-white text-xs py-3 px-10 rounded">Close</button>
+
+        </div>
+        <button onClick={onClose} className="block uppercase mx-auto shadow  bg-old-mauve hover:bg-cinnabar font-bold text-white focus:outline-none focus:ring-2 ring-chestnut text-xs py-3 px-10 rounded">Close</button>
       </div>
     </>,
     document.getElementById("portal")
