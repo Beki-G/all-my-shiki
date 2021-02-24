49226d8ade1fc223e4e723c223a8d38b9b10a738
Izuku <63758052+Beki-G@users.noreply.github.com>
2021-02-23 22:44:28 -0800

    Update row expansion styling in CharacterTable & TeamsTable

--- client/src/components/ShikigamiTableRowCard/ShikigamiTableRowCard.jsx
+++ client/src/components/ShikigamiTableRowCard/ShikigamiTableRowCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const ShikigamiTableRowCard = (row) => {
   const { original } = row;
@@ -21,36 +21,65 @@ const ShikigamiTableRowCard = (row) => {
 
   return (
     <div className="pb-2">
-      <div className="">
-        
-          <Link to={`/shiki/${original._id}`} className="text-gray-800 font-thin text-lg">{original.name}</Link>
-          <div className="flex flex-wrap -mx-px overflow-hidden md:-mx-1">
-            <div className= "my-px px-px w-full overflow-hidden sm:w-1/2 md:my-1 md:px-1 md:w-1/3">
-            Submitted By: {original.creatorId.userName}
-            </div>
-            <div className= "my-px px-px w-full overflow-hidden sm:w-1/2 md:my-1 md:px-1 md:w-1/3">
-            Base: {original.character.name}
-            </div>
-            <div className= "my-px px-px w-full overflow-hidden sm:w-1/2 md:my-1 md:px-1 md:w-1/3">
-            Date Created: {getFormattedDate(original.dateCreated)}
-            </div>
-            <div className= "my-px px-px w-full overflow-hidden sm:w-1/2 md:my-1 md:px-1 md:w-1/3">
-            Date Modified: {getFormattedDate(original.dateModified)}
-            </div>
-            <div className= "my-px px-px w-full overflow-hidden sm:w-1/2 md:my-1 md:px-1 md:w-1/3">
-            4 Soul Set: {original.soulsetMain?.name || "N/A"}
-            </div>
-            <div className= "my-px px-px w-full overflow-hidden sm:w-1/2 md:my-1 md:px-1 md:w-1/3">
-            2 Soul Set: {original.soulsetSub?.name || "N/A"}
-            </div>
-            <div className= "my-px px-px w-full overflow-hidden sm:w-1/2 md:my-1 md:px-1 md:w-1/3">
-            Slots 2/4/6: {original?.soulsetSlotTwo ||"N/A"}/{original?.soulsetSlotFour ||"N/A"}/{original?.soulsetSlotSix ||"N/A"}
-            </div>
-            <div className= "my-px px-px w-full overflow-hidden sm:w-1/2 md:my-1 md:px-1 md:w-1/3">
-            UserNotes: {original.userNotes ||"N/A"}
-            </div>
+      <div className="my-2 ">
+        <Link
+          to={`/shiki/${original._id}`}
+          className="text-gray-800 font-semibold text-lg  hover:text-purple-600 focus:text-purple-600"
+        >
+          {original.name}
+        </Link>
+        <div className="flex flex-wrap -mx-px overflow-hidden mt-2 md:-mx-1 font-medium sm:w-full  md:text-center lg:text-left lg:w-3/4 lg:mx-auto">
+          <div className="my-px px-px w-full overflow-hidden sm:w-1/2 md:px-1 md:w-1/3">
+            <span className="bg-old-mauve opacity-90 inline-block text-white rounded-md px-1 ">
+              Built By:
+            </span>{" "}
+            {original.creatorId.userName}
           </div>
-        
+          <div className="my-px px-px w-full overflow-hidden sm:w-1/2 md:px-1 md:w-1/3">
+            <span className="bg-old-mauve opacity-90 inline-block text-white rounded-md px-1 ">
+              Base:
+            </span>{" "}
+            {original.character.name}
+          </div>
+          <div className="my-px px-px w-full overflow-hidden sm:w-1/2 md:px-1 md:w-1/3">
+            <span className="bg-old-mauve opacity-90 inline-block text-white rounded-md px-1 ">
+              Date Created
+            </span>{" "}
+            {getFormattedDate(original.dateCreated)}
+          </div>
+          <div className="my-px px-px w-full overflow-hidden sm:w-1/2 md:px-1 md:w-1/3">
+            <span className="bg-old-mauve opacity-90 inline-block text-white rounded-md px-1 ">
+              Date Modified
+            </span>{" "}
+            {getFormattedDate(original.dateModified)}
+          </div>
+          <div className="my-px px-px w-full overflow-hidden sm:w-1/2 md:px-1 md:w-1/3">
+            <span className="bg-old-mauve opacity-90 inline-block text-white rounded-md px-1 ">
+              4 Soul Set:
+            </span>{" "}
+            {original.soulsetMain?.name || "N/A"}
+          </div>
+          <div className="my-px px-px w-full overflow-hidden sm:w-1/2 md:px-1 md:w-1/3">
+            <span className="bg-old-mauve opacity-90 inline-block text-white rounded-md px-1 ">
+              2 Soul Set:
+            </span>{" "}
+            {original.soulsetSub?.name || "N/A"}
+          </div>
+          <div className="my-px px-px w-full overflow-hidden sm:w-1/2 md:px-1 md:w-1/3">
+            <span className="bg-old-mauve opacity-90 inline-block text-white rounded-md px-1 ">
+              Slots 2/4/6:
+            </span>{" "}
+            {original?.soulsetSlotTwo || "N/A"}/
+            {original?.soulsetSlotFour || "N/A"}/
+            {original?.soulsetSlotSix || "N/A"}
+          </div>
+          <div className="my-px px-px w-full overflow-hidden sm:w-1/2 md:px-1 md:w-1/3">
+            <span className="bg-old-mauve opacity-90 inline-block text-white rounded-md px-1 ">
+              UserNotes:
+            </span>{" "}
+            {original.userNotes || "N/A"}
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/components/TeamsTableRowCard/TeamsTableRowCard.jsx
+++ client/src/components/TeamsTableRowCard/TeamsTableRowCard.jsx
@@ -1,15 +1,33 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const TeamsTableRowCard = (row) => {
   const { original } = row;
-//   console.log("original: ", original);
+
+  const dateFormat = (oldDate) => {
+    const tempDate = new Date(oldDate);
+    let newDate =
+      tempDate.getMonth() +
+      1 +
+      "/" +
+      tempDate.getDate() +
+      "/" +
+      tempDate.getFullYear().toString().substr(-2);
+    return newDate;
+  };
   return (
     <div className="flex mx-auto my-2 flex-col">
-        <div className="text-lg ">Title: 
-        <Link className="text-lg ml-1 hover:text-purple-700 focus:text-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-700" to={`/team/${original._id}`}>
-        {original.title}
-        </Link> </div>
+      <div className="text-lg font-semibold ">
+        <Link
+          className="text-lg font-semibold ml-1 hover:text-purple-600 focus:text-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-700"
+          to={`/team/${original._id}`}
+        >
+          Team name: {original.title}{" "}
+          <span className="font-normal text-base">
+            by {original.creatorId.userName}
+          </span>
+        </Link>{" "}
+      </div>
       <table className="table-auto childTable w-3/4 mx-auto my-2">
         <thead>
           <tr>
@@ -30,7 +48,17 @@ const TeamsTableRowCard = (row) => {
           })}
         </tbody>
       </table>
-        <div className="text-lg ">Notes: {original?.userNotes || "N/A"}</div>
+      <div className="my-2 flex flex-wrap justify-center md:flex-nowrap mx-auto p-4 w-3/4">
+        <div className="text-left w-1/2 mr-2">
+          <div><span className="font-semibold">Date Created:</span> {dateFormat(original.dateCreated)}</div>
+          {original.dateModified ? <div><span className="font-semibold">Date modified:</span> {dateFormat(original.dateModified)}</div> : ""}
+          <div><span className="font-semibold">User Notes:</span> {original?.userNotes || "N/A"}</div>
+        </div>
+        <div className="text-left w-1/2 ">
+          <div><span className="font-semibold">Likes:</span> {original.likes.length}</div>
+          <div><span className="font-semibold">User Guild:</span> {original.creatorId.guild}</div>
+        </div>
+      </div>
     </div>
   );
 };
